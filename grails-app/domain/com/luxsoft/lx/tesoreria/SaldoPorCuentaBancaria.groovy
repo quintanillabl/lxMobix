70857f08b7730420810af4c494621a65a887884e
Ruben Cancino <rubencancino6@gmail.com>
2015-07-07 15:35:36 -0500

    Saldo por cuenta bancaria

---
+++ grails-app/domain/com/luxsoft/lx/tesoreria/SaldoPorCuentaBancaria.groovy
@@ -0,0 +1,38 @@
+package com.luxsoft.lx.tesoreria
+
+import com.luxsoft.lx.core.Empresa
+
+class SaldoPorCuentaBancaria {
+
+	Empresa empresa
+
+	CuentaBancaria cuenta
+
+	BigDecimal saldoInicial
+
+	BigDecimal ingresos
+
+	BigDecimal egresos
+
+	BigDecimal saldoFinal
+
+	Integer ejercicio
+
+	Integer mes
+
+	Date cierre
+	
+	Date dateCreated
+	Date lastUpdated
+
+    static constraints = {
+    	ejercicio inList:(2015..2018)
+    	mes inList:(1..12)
+    	cuenta unique:['empresa','ejercicio','mes']
+		cierre(nullable:true)
+		
+
+    }
+
+    
+}
