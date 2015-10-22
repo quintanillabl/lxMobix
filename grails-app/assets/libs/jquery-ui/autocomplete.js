c620703722b9ddc72229b81c96b72ec258d4ff76
Ruben Cancino <rubencancino6@gmail.com>
2015-10-22 17:28:50 -0500

    Fix

--- grails-app/assets/libs/jquery-ui/autocomplete.js
+++ grails-app/assets/libs/jquery-ui/autocomplete.js
@@ -32,7 +32,7 @@ jQuery(function($){
 		dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
 		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
 		weekHeader: 'Sm',
-		dateFormat: 'dd/mm/yyyy',
+		dateFormat: 'dd/mm/yy',
 		firstDay: 1,
 		isRTL: false,
 		showMonthAfterYear: false,
