1579db3f0b7a87f5fea3ee68460f4e1df9f18249
Ruben Cancino <rubencancino6@gmail.com>
2015-10-26 18:05:54 -0600

    Fix 5

--- grails-app/services/com/luxsoft/lx/tesoreria/InversionService.groovy
+++ grails-app/services/com/luxsoft/lx/tesoreria/InversionService.groovy
@@ -11,68 +11,57 @@ class InversionService {
 	def movimientoDeCuentaService
 
     def  save(Inversion inversion) {
-		//def cuenta=inversion.cuentaOrigen
-		//inversion.cuentaDestino=cuenta
+		
 		assert inversion.cuentaOrigen!=inversion.cuentaDestino, 'No se puede hacer inversion sobre la misma cuenta'
 		
-		//inversion.vencimiento=inversion.fecha+inversion.plazo
 		inversion.comision=0
 		inversion.impuesto=0
-		//inversion.moneda=cuenta.moneda
-
+		
 		inversion.comentario=inversion.comentario?:'RE-INVERSION AUTOMATICA'
 		
-		MovimientoDeCuenta egreso=new MovimientoDeCuenta(
-			empresa:inversion.empresa,
-			cuenta:inversion.cuentaOrigen,
-			fecha:inversion.fecha,
-			importe:inversion.importe.abs()*-1.0,
-			concepto:inversion.comentario,
-			referencia:inversion.comentario,
-			comentario:'INVERSION'
-		)
-		
-		//Calculando la utilitdad
-		def rendimiento=0
+		if(inversion.importe.abs()){
+			MovimientoDeCuenta egreso=new MovimientoDeCuenta(
+				empresa:inversion.empresa,
+				cuenta:inversion.cuentaOrigen,
+				fecha:inversion.fecha,
+				importe:inversion.importe.abs()*-1.0,
+				concepto:inversion.comentario,
+				referencia:inversion.comentario,
+				comentario:'INVERSION'
+			)
+			movimientoDeCuentaService.preparar(egreso)
+			inversion.addToMovimientos(egreso)
 
-		def tasa=inversion.tasa
+			MovimientoDeCuenta ingreso=new MovimientoDeCuenta(
+				empresa:inversion.empresa,
+				cuenta:inversion.cuentaDestino,
+				fecha:inversion.vencimiento,
+				importe:importe.abs(),
+				concepto:inversion.comentario,
+				referencia:inversion.comentario,
+				comentario:'INVERSION'
+			)
+			movimientoDeCuentaService.preparar(ingreso)
+			inversion.addToMovimientos(ingreso)
 
-		def importe=inversion.importe
-
-		/*
-		def importeIsr=(inversion.importe*(inversion.tasaIsr/100))/inversion.cuentaOrigen.diasInversionIsr
-		importeIsr=importeIsr*inversion.plazo
-		if(tasa>0){
-			rendimiento=importe*(tasa/100)
-			rendimiento=(rendimiento/360)*inversion.plazo
-			rendimiento=rendimiento-importeIsr
 		}
-		*/
-
-		importe+=inversion.rendimientoReal
 		
-		//inversion.importeIsr=importeIsr
-		
-		inversion.rendimientoCalculado=rendimiento
-		//inversion.rendimientoReal=rendimiento
-		
-		//Generando el ingreso
-		MovimientoDeCuenta ingreso=new MovimientoDeCuenta(
-			empresa:inversion.empresa,
-			cuenta:inversion.cuentaDestino,
-			fecha:inversion.vencimiento,
-			importe:importe.abs(),
-			concepto:inversion.comentario,
-			referencia:inversion.comentario,
-			comentario:'INVERSION'
-
+		if(inversion.rendimientoReal.abs()){
 			
-		)
-		
-		movimientoDeCuentaService.preparar(ingreso)
-		movimientoDeCuentaService.preparar(egreso)
-		inversion.addToMovimientos(ingreso)
-		inversion.addToMovimientos(egreso)
+			
+			MovimientoDeCuenta rendimiento=new MovimientoDeCuenta(
+				empresa:inversion.empresa,
+				cuenta:inversion.cuentaDestino,
+				fecha:inversion.vencimiento,
+				importe:inversion.rendimientoReal.abs()+inversion.importeIsr.abs(),
+				concepto:inversion.comentario,
+				referencia:'INTERESE BRUTOS',
+				comentario:'INVERSION'
+			)
+			movimientoDeCuentaService.preparar(rendimiento)
+			inversion.addToMovimientos(rendimiento)
+			
+		}
 		
 		return inversion.save(failOnError:true)
 		
