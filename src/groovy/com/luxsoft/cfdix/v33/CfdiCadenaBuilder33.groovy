01aecb5412ec7db9f0c1fcc962ae4289a5eb951c
Ruben Cancino <rubencancino6@gmail.com>
2020-06-09 12:03:29 -0500

    Update CfdiCadenaBuilder33 para usar archivos locales

--- src/groovy/com/luxsoft/cfdix/v33/CfdiCadenaBuilder33.groovy
+++ src/groovy/com/luxsoft/cfdix/v33/CfdiCadenaBuilder33.groovy
@@ -1,5 +1,66 @@
 package com.luxsoft.cfdix.v33
 
+import groovy.util.logging.Slf4j
+import lx.cfdi.v33.CadenaBuilder33
+
+import javax.xml.transform.Transformer
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.stream.StreamSource
+
+import lx.cfdi.v33.Comprobante
+import lx.cfdi.v33.CfdiUtils
+
+@Slf4j
+class CfdiCadenaBuilder33 {
+    
+    String xsltUrl = "http://www.sat.gob.mx/sitio_internet/cfd/3/cadenaoriginal_3_3/cadenaoriginal_3_3.xslt"
+
+    private Transformer transformer;
+
+    String build(Comprobante comprobante){
+        log.debug('Generando cadena original para comprobante {}', comprobante.folio)
+        // Build transformer
+        Transformer transformer = getTransformer()
+
+        // Source
+        StreamSource xmlSource = buildSource(comprobante)
+
+        // Target
+        Writer writer = new StringWriter()
+        StreamResult target = new StreamResult(writer)
+
+        transformer.transform(xmlSource, target)
+        String cadena = writer.toString()
+        log.debug('Cadena original generada: {}', cadena)
+        return cadena
+    }
+    
+    Transformer getTransformer() {
+        if (!this.transformer) {
+            TransformerFactory factory=TransformerFactory.newInstance()
+            StreamSource source
+            if(Environment.current == Environment.DEVELOPMENT) {
+                source = new StreamSource(new File('/Users/rubencancino/dumps/xslt/cadenaoriginal_3_3.xslt'))
+            } else {
+                source = new StreamSource(new File('/home/xslt/cadenaoriginal_3_3.xslt'))
+            }
+            this.transformer = factory.newTransformer(source)
+        }
+        return this.transformer
+    }
+
+    StreamSource buildSource(Comprobante comprobante) {
+        String xml = CfdiUtils.serialize(comprobante)
+        Reader reader = new StringReader(xml)
+        return  new StreamSource(reader)
+    }
+
+}
+
+/** OLD FILE
+package com.luxsoft.cfdix.v33
+
 import javax.xml.transform.Source
 import javax.xml.transform.Transformer
 import javax.xml.transform.TransformerFactory
@@ -36,3 +97,5 @@ class CfdiCadenaBuilder33 {
 	}
 
 }
+
+**/
